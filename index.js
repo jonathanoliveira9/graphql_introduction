c2ccb2f58731c3fb7f31bf2f624b0221d3e45385
jonathanoliveira9 <jonathanoliveirasilva9@gmail.com>
2021-09-28 22:05:55 -0300

    type scalar

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const { gql, ApolloServer } = require( "apollo-server" )
-
-const resolvers = {
-  Query: {
-    hello() {
-      return 'World';
-    }
-  }
-}
+/**
+ * Scalar Types
+ * - Int
+ * - Float
+ * - String
+ * - Boolean
+ * - ID
+*/
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    idade: Int
+    salario: Float
+    nome: String
+    ativo: Boolean
+    id: ID
   }
 `
+
+const resolvers = {
+  Query: {
+    idade() {
+      return 18;
+    },
+    salario() {
+      return 5000.0
+    },
+    nome(){
+      return "GRAPHQL"
+    },
+    ativo(){
+      return true
+    },
+    id(){
+      return 11321312321;
+    }
+  }
+};
 
 const server = new ApolloServer({
   typeDefs,
